da38ad2731b1940267b6df079dc182d3c55f70a9
C-hayato <32670996+C-hayato@users.noreply.github.com>
2017-10-30 03:47:43 +0900

    Combined map and layout

---
+++ BrainStorming3/BrainStorming3/app/src/main/java/com/example/haruyai/brainstorming3/Mapping.java
@@ -0,0 +1,22 @@
+package com.example.haruyai.brainstorming3;
+
+import android.app.ActionBar;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+
+import com.example.h_sato.brainstorming3.R;
+
+/**
+ * Created by haruya.i on 2017/10/27.
+ */
+
+public class Mapping extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
